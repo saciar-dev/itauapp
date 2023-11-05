8ebdc4f9f513aed144cc9b79ce22dc672fe9c46a
Sergio Aciar <aciarenator@gmail.com>
2023-11-05 12:08:00 -0300

    primer commit

---
+++ src/i18n/pt/index.js
@@ -0,0 +1,29 @@
+export default {
+  port: 'Português',
+  esp: 'Espanhol',
+  init: 'Olá! Escolha seu idioma.',
+  next:'Seguindo',
+
+  firstCaption: 'Selecione o idioma',
+  secondStep:'Segundo passo',
+  back: 'Retornar',
+  finish:'Jogar',
+
+  //form
+  formTitle: 'Por favor, forneça os dados abaixo',
+  name:'Nome',
+  error: 'Por favor escreva algo',
+  email:'Email',
+  disclaim:'Eu aceito a cláusulas LGPD',
+  telephone:'Telefone',
+  acept: 'Aceitar',
+  reset: 'Excluir',
+
+    //game
+  gameReady: 'Vamos começar',
+  correct: 'Corretas',
+  msg_incorrect: 'Que pena, resposta incorreta!',
+  msg_correct: 'Resposta certa!',
+  msg_endless: 'Obrigado pela participação',
+  msg_result: 'Resultado: '
+}
